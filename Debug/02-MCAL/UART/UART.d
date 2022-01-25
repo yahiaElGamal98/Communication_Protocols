bfb46d97d227b938a0f79f4cd812efc593d65b68
YahiaElGamal <96386403+yahiaElGamal98@users.noreply.github.com>
2022-01-25 21:04:27 +0200

    Main Branch

---
+++ Debug/02-MCAL/UART/UART.d
@@ -0,0 +1,12 @@
+02-MCAL/UART/UART.d 02-MCAL/UART/UART.o: ../02-MCAL/UART/UART.c \
+ ../02-MCAL/UART/../../01-LIB/STD_types.h \
+ ../02-MCAL/UART/../../01-LIB/Registers.h \
+ ../02-MCAL/UART/UART_interface.h ../02-MCAL/UART/UART_config.h
+
+../02-MCAL/UART/../../01-LIB/STD_types.h:
+
+../02-MCAL/UART/../../01-LIB/Registers.h:
+
+../02-MCAL/UART/UART_interface.h:
+
+../02-MCAL/UART/UART_config.h:
---
+++ Debug/main.d
@@ -0,0 +1,6 @@
+main.d main.o: .././main.c ../././01-LIB/STD_types.h \
+ ../././02-MCAL/UART/UART_interface.h
+
+../././01-LIB/STD_types.h:
+
+../././02-MCAL/UART/UART_interface.h:
